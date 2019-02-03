597923d073d0ca57a9a297657beafcdd0358e5f0
Nils Ziehn <nziehn@googlemail.com>
2019-02-03 21:09:19 +0100

    fixed ci

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='readableID',
-    version='0.0.1',
+    version='0.0.2',
     description='Transform integer ids into readable, nonconsecutive ids',
     url='http://github.com/nziehn/readableID',
     author='Nils Ziehn',
