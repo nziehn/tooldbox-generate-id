644d2c2181afd21a18929b22665389a60e05f292
Nils Ziehn <nziehn@googlemail.com>
2019-02-03 21:14:44 +0100

    add README to PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+from os import path
 from setuptools import setup
+
+# read the contents of your README file
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='readableID',
-    version='0.0.2',
+    version='0.0.3',
     description='Transform integer ids into readable, nonconsecutive ids',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://github.com/nziehn/readableID',
     author='Nils Ziehn',
     author_email='nziehn@mail.com',
